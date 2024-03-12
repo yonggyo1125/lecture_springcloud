cb489f40c8884f01ec86f572614812f51cc14ec0
yonggyo1125 <yonggyo00@kakao.com>
2024-03-12 17:24:34 +0900

    스프링 클라우드 스트림 강의 작성

--- 6. 스프링 클라우드 게이트웨이를 이용한 서비스 라우팅/source/board-service/src/main/java/org/choongang/board/BoardServiceApplication.java
+++ 6. 스프링 클라우드 게이트웨이를 이용한 서비스 라우팅/source/board-service/src/main/java/org/choongang/board/BoardServiceApplication.java
@@ -2,8 +2,11 @@ package org.choongang.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
+@EnableBinding(Source.class)
 public class BoardServiceApplication {
 
 	public static void main(String[] args) {
